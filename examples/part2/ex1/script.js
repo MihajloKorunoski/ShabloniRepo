adb7849e1904c798538afe3420d93bd8ec73f393
Mihaylo Markov Korunoski <mihajlo_korunoski@icloud.com>
2024-08-18 03:15:44 +0200

    a new exercise finished

---
+++ examples/part2/ex1/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    let pull = $('#pull');
+    let menu = $('nav ul');
+
+    $(pull).on('click',function (e){
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function (){
+       let w = $(window).width();
+       if(w > 600 && menu.is(':hidden')){
+           menu.removeAttr('style');
+       }
+    });
+
+    $('nav a').click(function (event){
+        event.preventDefault();
+        let targetPage = $(this).attr('href');
+
+        $('#items').fadeOut(300, function (){
+            $(this).load(targetPage + " #items").fadeIn(300);
+        });
+    });
+
+    $("nav a").hover(function (){
+        $(this).toggleClass("active");
+    });
+
+    $(".content").hover(function (){
+        $(this).effect("bounce", { times: 3}, 300);
+    });
+})
